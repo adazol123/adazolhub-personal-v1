c29dc658e5a58a6697f85fc40d6cab95c9f1919f
Daniel Lozada <37337887+adazol123@users.noreply.github.com>
2023-06-09 22:05:54 +0800

    fix build file and integrated posthog (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery
+} from '@tanstack/react-query'
 import { AnimatePresence } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
@@ -15,47 +19,63 @@ const Learning = React.lazy(() => import('./pages/learning'))
 const Projects = React.lazy(() => import('./pages/projects'))
 const UXCaseStudy = React.lazy(() => import('./pages/ux-case-study'))
 import './styles/App.css'
-import LearningLayout from './layouts/LearningLayout';
+import LearningLayout from './layouts/LearningLayout'
 import FormStep from './components/learning/FormStep'
 import GettingStarted from './components/learning/GettingStarted'
-import useScrollToTop from './utils/helper/useScrollToTop';
+import useScrollToTop from './utils/helper/useScrollToTop'
 import ImagePreviewWithFileReader from './components/learning/ImagePreviewWithFileReader'
-import DeviceDetector from 'device-detector-js';
-import { incrementCount } from './api/firebase';
+import DeviceDetector from 'device-detector-js'
+import { incrementCount } from './api/firebase'
+import { posthog } from 'posthog-js'
 
 const queryClient = new QueryClient()
 
-function App() {
+function App () {
   useScrollToTop()
-  const [count, setCount] = useState(0)
   const menuState = useToggleStore(state => state.menu)
-  
+
+  posthog.init('phc_NyNxKAIaFxHp4MAXT8QdiA9QtjH1ZVDtP7mS4DKTCot', {
+    api_host: 'https://app.posthog.com'
+  })
+
+  // /api/organizations/@current
+  posthog.capture('my event', { property: 'value' })
+
   return (
     <QueryClientProvider client={queryClient}>
-      <React.Suspense fallback={<div className='min-h-screen grid place-content-center place-items-center gap-2'>
-        <h5>ADAZOLHUB</h5>
-        <span className='relative h-6 w-16'>
-          <img className='absolute inset-0' src='/images/1484.gif' alt='loading...' />
-
-        </span>
-      </div>}>
-
+      <React.Suspense
+        fallback={
+          <>
+            <div className='min-h-screen grid place-content-center place-items-center gap-2'>
+              <h5>ADAZOLHUB</h5>
+              <span className='relative h-6 w-16'>
+                <img
+                  className='absolute inset-0'
+                  src='/images/1484.gif'
+                  alt='loading...'
+                />
+              </span>
+            </div>
+          </>
+        }
+      >
         <main>
-
-
           <HeaderNav />
           <Routes>
             <Route index element={<Home />} />
-            <Route path='/introduction' >
+            <Route path='/introduction'>
               <Route index element={<Introduction />} />
               <Route path='resume' element={<Resume />} />
             </Route>
             <Route path='/projects' element={<Projects />} />
             <Route path='/ux-case-study' element={<UXCaseStudy />} />
-            <Route path='/learning' element={<Learning />} >
+            <Route path='/learning' element={<Learning />}>
               <Route index element={<GettingStarted />} />
               <Route path='zustand' element={<LearningLayout />} />
-              <Route path='image-preview' element={<ImagePreviewWithFileReader />} />
+              <Route
+                path='image-preview'
+                element={<ImagePreviewWithFileReader />}
+              />
               <Route path='form-stepper' element={<FormStep />} />
             </Route>
             <Route path='/contact' element={<Contact />} />
@@ -63,7 +83,6 @@ function App() {
           <FooterContact />
         </main>
         <AnimatePresence mode='sync'>
-
           {menuState && <NavOverlay />}
         </AnimatePresence>
       </React.Suspense>
--- src/components/header/nav/HeaderNav.tsx
+++ src/components/header/nav/HeaderNav.tsx
@@ -4,10 +4,22 @@ import { Link, NavLink, useLocation } from 'react-router-dom'
 import useMeasure from 'react-use-measure';
 import NavLinksMobile from '../navlinks/NavLinksMobile'
 import NavLinksDesktop from './../navlinks/NavLinksDesktop';
+import { useQuery } from '@tanstack/react-query';
+import DeviceDetector from 'device-detector-js';
+import { incrementCount } from '../../../api/firebase';
 
+const fetchData = async () => {
+    const dd = new DeviceDetector();
+    const userAgent  = navigator.userAgent;
+    const device = dd.parse(userAgent)
+    return await incrementCount({ device })
+  }
 const HeaderNav = () => {
     const [ref, { width }] = useMeasure({ polyfill: ResizeObserver })
     const { pathname } = useLocation()
+    const { data, error } = useQuery(['unique_visit'], fetchData, { refetchOnWindowFocus: false, cacheTime: 120000 })
+
+
     return (
         <>
             <header ref={ref}>
