f4d94d6743d61a94a207776abceafcc5d42ba4e8
Danyel Lozada <37337887+adazol123@users.noreply.github.com>
2023-06-09 23:43:35 +0800

    convert posthog init to react configuration

--- src/App.tsx
+++ src/App.tsx
@@ -24,69 +24,71 @@ import FormStep from './components/learning/FormStep'
 import GettingStarted from './components/learning/GettingStarted'
 import useScrollToTop from './utils/helper/useScrollToTop'
 import ImagePreviewWithFileReader from './components/learning/ImagePreviewWithFileReader'
-import DeviceDetector from 'device-detector-js'
-import { incrementCount } from './api/firebase'
-import { posthog } from 'posthog-js'
-
+import { PostHogProvider } from 'posthog-js/react'
+const options = {
+  api_host: 'https://app.posthog.com'
+}
 const queryClient = new QueryClient()
 
 function App () {
   useScrollToTop()
   const menuState = useToggleStore(state => state.menu)
 
-  posthog.init('phc_NyNxKAIaFxHp4MAXT8QdiA9QtjH1ZVDtP7mS4DKTCot', {
-    api_host: 'https://app.posthog.com'
-  })
 
   // /api/organizations/@current
-  posthog.capture('my event', { property: 'value' })
+  // posthog.capture('my event', { property: 'value' })
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <React.Suspense
-        fallback={
-          <>
-            <div className='min-h-screen grid place-content-center place-items-center gap-2'>
-              <h5>ADAZOLHUB</h5>
-              <span className='relative h-6 w-16'>
-                <img
-                  className='absolute inset-0'
-                  src='/images/1484.gif'
-                  alt='loading...'
+    <PostHogProvider
+      apiKey='phc_NyNxKAIaFxHp4MAXT8QdiA9QtjH1ZVDtP7mS4DKTCot'
+      options={options}
+    >
+      <QueryClientProvider client={queryClient}>
+        <React.Suspense
+          fallback={
+            <>
+              <div className='min-h-screen grid place-content-center place-items-center gap-2'>
+                <h5>ADAZOLHUB</h5>
+                <span className='relative h-6 w-16'>
+                  <img
+                    className='absolute inset-0'
+                    src='/images/1484.gif'
+                    alt='loading...'
+                  />
+                </span>
+              </div>
+            </>
+          }
+        >
+          <main>
+            <HeaderNav />
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='/introduction'>
+                <Route index element={<Introduction />} />
+                <Route path='resume' element={<Resume />} />
+              </Route>
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/ux-case-study' element={<UXCaseStudy />} />
+              <Route path='/learning' element={<Learning />}>
+                <Route index element={<GettingStarted />} />
+                <Route path='zustand' element={<LearningLayout />} />
+                <Route
+                  path='image-preview'
+                  element={<ImagePreviewWithFileReader />}
                 />
-              </span>
-            </div>
-          </>
-        }
-      >
-        <main>
-          <HeaderNav />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='/introduction'>
-              <Route index element={<Introduction />} />
-              <Route path='resume' element={<Resume />} />
-            </Route>
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/ux-case-study' element={<UXCaseStudy />} />
-            <Route path='/learning' element={<Learning />}>
-              <Route index element={<GettingStarted />} />
-              <Route path='zustand' element={<LearningLayout />} />
-              <Route
-                path='image-preview'
-                element={<ImagePreviewWithFileReader />}
-              />
-              <Route path='form-stepper' element={<FormStep />} />
-            </Route>
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-          <FooterContact />
-        </main>
-        <AnimatePresence mode='sync'>
-          {menuState && <NavOverlay />}
-        </AnimatePresence>
-      </React.Suspense>
-    </QueryClientProvider>
+                <Route path='form-stepper' element={<FormStep />} />
+              </Route>
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+            <FooterContact />
+          </main>
+          <AnimatePresence mode='sync'>
+            {menuState && <NavOverlay />}
+          </AnimatePresence>
+        </React.Suspense>
+      </QueryClientProvider>
+    </PostHogProvider>
   )
 }
 
